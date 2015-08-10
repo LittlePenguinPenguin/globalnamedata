28d8a2e7b9337d682fd9d9b721d0ff0bc983915d
Pingyuan Zheng <zoe@Administrators-MacBook-Pro-2.local>
2015-08-10 15:23:29 -0700

    Modify nameBinom function

--- R/classifier.R
+++ R/classifier.R
@@ -23,38 +23,41 @@
 #'
 #' @export
 #' @importFrom binom binom.confint
-nameBinom <- function(data, method = "ac", 
-                      threshold = 0.9, ...) {
-  base.cols <- c("Name", "years.appearing","count.female", "count.male")
-  # drop columns from other analyses
-  data <- data[, names(data) %in% base.cols]
-
-  pred <- with(data, binom.confint(count.male, count.male + count.female,
-                                   method = method, ...))
-
-  # assign as male/female if we meet the thresholds
-  procThreshold <- function(input, threshold) {
-    if (threshold < 0.5) {
-      treshold <- 1 - threshold
+nameBinom <- function (data, method = "ac", threshold = 0.9, ...) 
+{
+    base.cols <- c("Name", "years.appearing", "count.female", 
+        "count.male")
+    data <- data[, names(data) %in% base.cols]
+    unknown_count <- 0
+    f <- function(data, threshold = 0.9, method = "ac") {
+        totalcount <- as.numeric(data["count.male"]) + as.numeric(data["count.female"])
+        pred <- binom.confint(as.numeric(data["count.male"]), 
+            totalcount, method = method)
+        prob <- pred[, "mean"]
+        lower <- pred[, "lower"]
+        upper <- pred[, "upper"]
+        if (prob > threshold) {
+            out.val <- "Male"
+        }
+        else if (prob < 1 - threshold) {
+            out.val <- "Female"
+        }
+        else {
+            unknown_count <<- unknown_count + 1
+            trial <- rbinom(1, 10, prob)
+            if (trial >= 10 * lower & trial <= 10 * upper) {
+                out.val <- "Male"
+            }
+            else {
+                out.val <- "Female"
+            }
+        }
+        return(out.val)
     }
-    out.val <- rep("Unknown", times = length(input))
-    out.val <- ifelse(input > threshold, 
-                      "Male", ifelse(input < 1 - threshold,
-                                     "Female", out.val))
-    return(out.val)
-  }
-
-  gender.prediction <- procThreshold(pred[, "mean"], threshold)
-  gender.observed <- with(data, (count.male / (count.male + count.female)))
-  # Because the ratios are essentially symmetric we can assign
-  # confidence intervals for both in one pass
-  gender.upper <- with(pred, ifelse(mean > 0.5, upper, 1 - lower))
-  gender.lower <- with(pred, ifelse(mean > 0.5, lower, 1 - upper))
-
-  data.pred <- data.frame(prob.gender = gender.prediction,
-                          obs.male = gender.observed,
-                          est.male = pred[, "mean"],
-                          upper = gender.upper,
-                          lower = gender.lower)
-  return(cbind(data, data.pred))
+    gender.prediction <- apply(data, 1, f)
+    gender.observed <- with(data, (count.male/(count.male + count.female)))
+    print(unknown_count)
+    data.pred <- data.frame(prob.gender = gender.prediction, 
+        obs.male = gender.observed)
+    return(cbind(data, data.pred))
 }
